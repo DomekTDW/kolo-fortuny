ab8605e4ad1d8ec5cfdadca5cf99b464eb8f18a8
DomekTDW <domektdw@gmail.com>
2025-08-01 16:12:57 +0200

    Dodano AuthController z rejestracją i logowaniem

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function spins()
+    {
+        return $this->hasMany(Spin::class);
+    }
 }
